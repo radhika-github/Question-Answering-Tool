0270e2e923037e330ed4f302888307d0f945b9ba
Radhika <radhi.virgo.16@gmail.com>
2020-04-29 02:27:50 -0500

    Refactoring

--- kb.pl
+++ kb.pl
@@ -1,3 +1,5 @@
+% Beef is a type of meat
+typeOf(beef,meat).
 typeOf(sausage_meat,meat).
 typeOf(stockfish,meat).
 typeOf(anchovy,meat).
@@ -86,7 +88,6 @@ typeOf(lamb,meat).
 typeOf(capon,meat).
 typeOf(cold_cuts,meat).
 typeOf(bully_beef,meat).
-typeOf(beef,meat).
 typeOf(red_meat,meat).
 typeOf(scallop,meat).
 typeOf(fish,meat).
@@ -119,42 +120,48 @@ typeOf(soap,skincare).
 typeOf(tomatoes,vegetable).
 %Deodrant is type of skincare
 typeOf(deodorant,skincare).
+% Credit card is a type of money
 typeOf(creditcard,money).
-typeOf(money,money).
+% Cash is a type of money
 typeOf(cash,money).
-%Yesterday John went to the North Berkeley Safeway supermarket and bought two
-%pounds of tomatoes and a pound of ground beef
-
-
-%buys(john,beef).
+typeOf(money,money).
 
 %-----------------------------------------------------------------------------------------------------------------------
 
+%John shopped beef at North Berkeley Safeway yesterday
+shop(john,beef,northBerkeleySafeway,yesterday).
+
+%John shopped tomatoes at North Berkeley Safeway yesterday
+shop(john,tomatoes,northBerkeleySafeway,yesterday).
+
 %John lives alone
 livesAlone(john).
+
 %Anyone who lives alone shops alone
 shopsAlone(X):-livesAlone(X).
-%If someone shops alone and if he shops then he is adult
+
+%If someone has to shop alone and shops, then it’s an adult.
 isa(X,adult):-shop(X,_,_,_),shopsAlone(X).
+
 %If someone is not an adult, then he is a child
 isa(X,child):- !,\+ isa(X,adult).
+
 %If someone is not an adult, then he is a child
 isa(X,child):- isa(X,adult), !, fail.
 
 %-----------------------------------------------------------------------------------------------------------------------
 
 % Tomatoes have a unit weight of  0.5 pounds
-unitWeight(tomatoes,0.5).
+weightPerUnitOf(tomatoes,0.5).
+weightPerUnitOf(beef,1).
 
 % If John shopped for tomatoes, he bought 2 pounds of tomatoes and 1 pound of beef.
 pounds(john,1,beef):-shop(john,beef,northBerkeleySafeway,yesterday).
 pounds(john,2,tomatoes):-shop(john,tomatoes,northBerkeleySafeway,yesterday).
 pounds(mary,1,tomatoes):-shop(mary,tomatoes,northBerkeleySafeway,yesterday).
-shop(john,beef,northBerkeleySafeway,yesterday).
-shop(john,tomatoes,northBerkeleySafeway,yesterday).
 
 % The number of tomatoes that john has is obtained by the no. of pounds/unit weight.
-number(P,X,I):-unitWeight(I,Y),pounds(P,Z,I),X is Z/Y.
+number(P,X,I):-weightPerUnitOf(I,Y),pounds(P,Z,I),X is Z/Y.
 
 % If N>=M then N is larger or equal to M.
 largerorequal(N,M):-N>=M.
@@ -162,7 +169,7 @@ largerorequal(N,M):-N>=M.
 % If John has some number of tomatoes and if it is larger or equal to some number then he has atleast that many number of tomatoes.
 hasatleast(P,Y,I):- number(P,X,I),largerorequal(X,Y).
 
-%--------------------------------------------------------------------------------------
+%-----------------------------------------------------------------------------------------------------------------------
 
 % If someone shopped for something somewhere sometime, then they bought that thing.
 bought(X,Y):-shop(X,Y,_,_).
@@ -170,9 +177,9 @@ bought(X,Y):-shop(X,Y,_,_).
 % If John bought something and if that thing is meat then John bought meat
 buys(X,Y):-bought(X,Z), typeOf(Z,Y).
 
-%---------------------------------------------------------------------------------------
+%-----------------------------------------------------------------------------------------------------------------------
 
-% Mary was buying tomatoes at northBerkeleySafeway at the same time as john.
+% Mary was buying tomatoes at North Berkeley Safeway at the same time as john.
 shop(mary,tomatoes,northBerkeleySafeway,T):-shop(john,tomatoes,northBerkeleySafeway,T).
 
 % If someone buys something at some store at sometime then that person is at that store at that time.
@@ -181,7 +188,8 @@ isAtStore(X,Y,Z):-shop(X,_,Y,Z).
 % If someone buys something at some stall at sometime then that person is at that stall at that time.
 isAtStall(X,Y,Z):-shop(X,Y,_,Z).
 
-% If someone is at some stall at some store, and if someone else is also at the  same stall and same store, then that person sees the other person
+% If someone is at some stall at some store, and if someone else is also at the same stall and same store,
+% then that person sees the other person
 sees(X,V):-isAtStore(X,Y,Z),isAtStall(X,U,Z),isAtStore(V,Y,Z),isAtStall(V,U,Z).
 
 % Or else they will not see each other.
@@ -190,17 +198,20 @@ sees(X,V):-!, \+isAtStore(X,Y,Z); \+isAtStall(X,U,Z); \+isAtStore(V,Y,Z); \+isAt
 % If X sees Y then Y also sees X.
 sees(Y,X):-sees(X,Y).
 
-%--------------------------------------------------------------------------------------------
+%-----------------------------------------------------------------------------------------------------------------------
 
 % If something is a vegetable then it originates from nature.
 originates(X,nature):-typeOf(X,vegetable).
+
 % If something is a skincare then it does not originate in nature.
 originates(X,nature):-typeOf(X,skincare), !, fail.
+
 %If something is of form something which orginates from man, then it is made in the supermarket.
 made(X,supermarket):- !,\+originates(X,nature).
-%---------------------------------------
 
-% If something is a Vegetable or meat, it can be eaten.
+%-----------------------------------------------------------------------------------------------------------------------
+
+% If something is a vegetable or meat, it can be eaten.
 can(X,eaten):-typeOf(X,vegetable);typeOf(X,meat).
 
 % Precedence of days
@@ -216,9 +227,9 @@ has(X,Y,T):-shop(X,Y,_,T).
 % Someone eats something at sometime if someone has something at that time and if that thing can be eaten.
 whatWill(X,Y,T,eatThem):- has(X,Y,K), can(Y,eaten),after(T,K).
 
-%-------------------------------------------------------------
+%-----------------------------------------------------------------------------------------------------------------------
 
-% Tops has a branch called Tops Maple road supermarket
+% Safeway has a branch called North Berkeley Safeway supermarket
 hasbranch(safeway,northBerkeleySafeway).
 
 % Skin care products, vegetables, meat are available in supermarkets.
@@ -230,7 +241,7 @@ available(vegetable,supermarket).
 sells(X,Y):- hasbranch(X,U),typeOf(U,supermarket),typeOf(Y,Z),available(Z,supermarket);
 hasbranch(X,U),typeOf(U,supermarket),available(Y,supermarket).
 
-%---------------------------------------------------------------------
+%-----------------------------------------------------------------------------------------------------------------------
 
 % If anyone shops at someplace for anything at some times then they pay money for that thing at that time at that place.
 pay(X,Z,Y,T):-shop(X,_,Y,T).
@@ -238,7 +249,7 @@ pay(X,Z,Y,T):-shop(X,_,Y,T).
 % If someone has to pay money at sometime at a store which is of some type then they must bring that money to that place at that time.
 bring(X,Z,Y,T):-pay(X,Z,K,T),typeOf(K,Y),typeOf(Z,money).
 
-%------------------------------------------
+%-----------------------------------------------------------------------------------------------------------------------
 
 % If anyone shops at someplace for anything at some times then they pay money for that thing at that time at that place.
 % If someone who pays money at sometime at someplace of some type then he spent money that time in that type of place
@@ -251,15 +262,33 @@ after(yesterday,before).
 % If someone spend money at someplace at some time then he had less money after that time at the same place.
 moneyLeft(W,less,Y,T):- spentMoney(W,Y,U),after(T,U).
 
-%------------------------------------------
+%-----------------------------------------------------------------------------------------------------------------------
+
+%If a person shops at supermarket then the supermarket is open at that time.
+isOpen(P,L,T):- shop(P,_,L,T).
+
+%If the supermarket is open then the staff is present
+staffPresent(P,L,T):-isOpen(P,L,T).
+
+% When a person is at the supermarket and supermarket is open any item and staff is people
+% then there are other people in supermarket
+peopleAtStore(P,L,T):-isAtStore(P,L,T),isOpen(P,L,T),staffPresent(P,L,T).
+
+%-----------------------------------------------------------------------------------------------------------------------
 
 % If someone shops meat then he is non vegeterian
-isNonVeg(X):- shop(X,Y,_,_),typeOf(Y,meat).
+nonVeg(X):- shop(X,Y,_,_),typeOf(Y,meat).
 
 % If someone is not a non vegeterian then he is vegeterian
-isVeg(X):-!,\+isNonVeg(X).
+veg(X):-!,\+nonVeg(X).
 
-%------------------------------------------
+%-----------------------------------------------------------------------------------------------------------------------
+
+% If someone sells something then he owns that thing
+owns(X,Y):-sells(X,Y).
+
+%-----------------------------------------------------------------------------------------------------------------------
+
 % If someone shopped 1 pound of something, then he bought 16 ounces of that thing.
 ounces(X,Y,Z):-pounds(X,W,Z), Y is W*16.
 
@@ -283,18 +312,5 @@ spaceOccupied(P,I,X):- pounds(P,W,I),volume(I,V), X is W*V.
 %If the space occupied by the number of pounds of item bought is than or equal to car trunk capacity of the person
 %then the item fits in car.
 fitInCar(P,W):- spaceOccupied(P,W,Y),carTrunkCapacity(P,X),largerorequal(X,Y).
-
-%-----------------------------------------------------------------------------------------------------------------------
-
-%If a person shops at supermarket then the supermarket is open at that time.
-%If the supermarket is open then the staff is present
-%Hence when a person is at the supermarket and shops any item then there are other people in safeway
-isOpen(P,L, T):- isAtStore(P,L,T).
-staffPresent(P,L,T):-isOpen(P,L,T).
-peopleAtStore(P,L,T):-staffPresent(P,L,T).
-
-%-----------------------------------------------------------------------------------------------------------------------
-
-owns(X,Y):-sells(X,Y).
 
 %-----------------------------------------------------------------------------------------------------------------------
